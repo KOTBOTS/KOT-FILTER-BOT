8399f06e286b7fd4652f4feba6b83d6e9c060364
Hurricane-Hari <87891571+Hurricane-Hari@users.noreply.github.com>
2021-09-11 17:40:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name}💋Join🏹KOT_LINKS_TEAM.",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
